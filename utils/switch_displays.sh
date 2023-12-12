8a020ee6c9fe212510c042b6a1327a00233ef06c
Yang Li <yanglited@gmail.com>
2023-12-11 21:41:05 -0500

    Working display switching script

--- utils/switch_displays.sh
+++ utils/switch_displays.sh
@@ -1,2 +1,17 @@
-xrandr --output "DP-1" --mode 3440x1440 --output "eDP-1" --off
-xrandr --output "eDP-1" --mode 1920x1080 --output "DP-1" --off
+#!/bin/bash
+
+# WIP: automatic switching not working yet.
+# Create a rule: /etc/udev/rules.d/99-external-screen.rules with following in the rule file:
+# SUBSYSTEM=="drm", ACTION=="change", RUN+="/home/yli/utils/switch_display.sh"
+# Then use following command to reload the rules:
+# sudo udevadm control --reload-rules
+# Related: sudo cat /sys/class/drm/card1-HDMI-A-1/status
+# connected
+
+export DISPLAY=:0
+export XAUTHORITY=/home/yli/.Xauthority
+if xrandr | grep "HDMI-1 connected"; then
+    ./use_hdmi-1.sh
+else
+    ./use_edp-1.sh
+fi
---
+++ utils/use_hdmi-1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output HDMI-1 --auto --output eDP-1 --off
