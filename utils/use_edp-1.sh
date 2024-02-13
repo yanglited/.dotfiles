40814465123563f3c70a2a2f9b378df4c7bc7b2c
Yang Li <yanglited@gmail.com>
2024-02-13 13:40:46 -0500

    update to util to use only eDP-1

--- utils/use_edp-1.sh
+++ utils/use_edp-1.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 xrandr --output HDMI-1 --off  --output eDP-1 --mode 1920x1080
+
+# Get the name of the built-in display (you might want to adjust this)
+builtin_display="eDP-1"
+
+# Get a list of connected displays (excluding the built-in display)
+connected_displays=$(xrandr | grep "connected" | grep -v "$builtin_display" | awk '{print $1}')
+
+# Disconnect each connected display
+for display in $connected_displays; do
+    xrandr --output "$display" --off
+done; xrandr --output "$builtin_display" --mode 1920x1080
